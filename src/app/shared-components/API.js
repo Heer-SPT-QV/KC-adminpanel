390a38b64c72e2d114877e531f3b0f1caa684508
Meer <meer.spt@gmail.com>
2021-09-10 15:47:01 +0530

    meta

--- src/app/shared-components/API.js
+++ src/app/shared-components/API.js
@@ -1,4 +1,4 @@
-export const API = 'https://korean-backend.ap-south-1.elasticbeanstalk.com';
+export const API = 'http://korean-backend.ap-south-1.elasticbeanstalk.com';
 // export const API = 'http://192.168.29.120:8080';
 // export const API = 'https://qwertyvate.herokuapp.com/';
 // export const API2 = 'https://qwertyvate.herokuapp.com';
