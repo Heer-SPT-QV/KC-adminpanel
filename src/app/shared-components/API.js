40561fb0e4fea9ec74d5957894bf02ec3802150b
Meer <meer.spt@gmail.com>
2021-10-11 15:04:03 +0530

    https again

--- src/app/shared-components/API.js
+++ src/app/shared-components/API.js
@@ -3,6 +3,7 @@ export const API = 'https://api.koreanconvenienceapp.com';
 
 // export const API = 'http://192.168.29.120:8080';
 // export const API = 'https://qwertyvate.herokuapp.com/';
+// export const API = 'https://qwertyvate.herokuapp.com/';
 // export const API2 = 'https://qwertyvate.herokuapp.com';
 // export const APILOGIN=""
 export const header = {};
